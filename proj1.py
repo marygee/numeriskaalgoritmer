b979162a2e367303fd2cd5a4421a3df6eae569ba
snugo <47242280+snugo@users.noreply.github.com>
2019-09-16 11:44:26 +0200

    Add files via upload
    Lade till add-on 1.

--- proj1.py
+++ proj1.py
@@ -5,31 +5,24 @@ Created on Thu Sep 12 10:29:18 2019
 """
 from  scipy import *
 from  pylab import *
+import  unittest
 
 class bspline:
     def __init__(self, coord, u_grid):
         self.coord = coord
         k = len(self.coord)
         self.u_grid = u_grid
-#        N0 = empty(k)
-#        N1 = empty(k)
-#        N2 = empty(k)
-#        N3 = empty(k)
-#        for i in range([1, k]): # den ska inte inkludera 0??
-#            N0[i] = lambda x : heaviside(x - u_grid[i-1], 1) - heaviside(x - u_grid[i], 1)
-#        for i in range([1, k]):
-#            N1[i] = lambda x : (x - u_grid[i-1])/(u_grid[i+1-1]-u_grid[i-1])*N0[i] + (u_grid[i+1]-x)/(u_grid[i+1]-u_grid[i])*N0[i+1]
-#        for i in range([1, k]):
-#            N2[i] = lambda x : (x - u_grid[i-1])/(u_grid[i+2-1]-u_grid[i-1])*N1[i] + (u_grid[i+2]-x)/(u_grid[i+2]-u_grid[i])*N1[i+1]
-#        for i in range([1, k]):
-#            N3[i] = lambda x : (x - u_grid[i-1])/(u_grid[i+3-1]-u_grid[i-1])*N2[i] + (u_grid[i+3]-x)/(u_grid[i+3]-u_grid[i])*N2[i+1]
-        
-    def __call__(self, u, d):
+
+    def __call__(self, u, d, blossom_i):
         self.u = u
         self.d = d
         i_hot = self.findhot(u)
-        s = self.blossom(self.d, self.u, i_hot)
-        self.plot()
+        [d_hotx, d_hoty] = self.blossom(self.d, self.u, i_hot)
+        sx = d_hotx[3, 0]
+        sy = d_hoty[3, 0]
+        s = (sx, sy)
+        self.plot(blossom_i)
+        print('s(', self.u, ') =', s)
         return s
     
     def findhot(self, u):
@@ -50,9 +43,10 @@ class bspline:
                 alpha = self.alpha(u, i_hot, t, index)
                 d_hotx[t+1, index] = alpha*d_hotx[t, index] + (1-alpha)*d_hotx[t, index+1]
                 d_hoty[t+1, index] = alpha*d_hoty[t, index] + (1-alpha)*d_hoty[t, index+1]
-        sx = d_hotx[3, 0]
-        sy = d_hoty[3, 0]
-        return (sx, sy)
+        return [d_hotx, d_hoty]
+#        sx = d_hotx[3, 0]
+#        sy = d_hoty[3, 0]
+#        return (sx, sy)
         
     def alpha(self, u, i_hot, t, index):
         i_leftmost = i_hot - 2 + index + t
@@ -61,18 +55,103 @@ class bspline:
         u_rightmost = self.u_grid[i_rightmost]
         return (u_rightmost - u) / (u_rightmost - u_leftmost)
     
-    def plot(self):
+    def plot(self, blossom_i):
         import matplotlib.pyplot as plt
         x_val = [x[0] for x in self.coord]
         y_val = [x[1] for x in self.coord]
         plt.plot(x_val,y_val,'or')
-        u = linspace(0.0000001, 0.999999, 100)
-        s = zeros((100, 2))
-        for j in range(0, 100):
+        u = linspace(0.0000001, 0.999999, 1000)
+        s = zeros((1000, 2))
+        blossom_pointsx = zeros(4)
+        blossom_pointsy = zeros(4)
+        for j in range(0, 1000):
             i_hot = self.findhot(u[j])
-            s[j, :] = self.blossom(self.d, u[j], i_hot)
+            [d_hotx, d_hoty] = self.blossom(self.d, u[j], i_hot)
+            sx = d_hotx[3, 0]
+            sy = d_hoty[3, 0]
+            s[j, :] = (sx, sy)
         plt.plot(s[:,0], s[:,1])
+        self.plotblossom(blossom_i)
         plt.show()
+        
+    def plotblossom(self, blossom_i): # Add-on 1
+        i_hot = self.findhot(self.u_grid[blossom_i])
+        u = linspace(self.u_grid[i_hot]*0.95, self.u_grid[i_hot+1]*1.05, 100)
+        d1 = zeros((100, 2))
+        d2 = zeros((100, 2))
+        d3 = zeros((100, 2))
+        for j in range(0, 100):
+            [d_hotx, d_hoty] = self.blossom(self.d, u[j], i_hot)
+            d1x = d_hotx[1, 0]
+            d1y = d_hoty[1, 0]
+            d1[j, :] = (d1x, d1y)
+            d2x = d_hotx[2, 0]
+            d2y = d_hoty[2, 0]
+            d2[j, :] = (d2x, d2y)
+            d3x = d_hotx[3, 0]
+            d3y = d_hoty[3, 0]
+            d3[j, :] = (d3x, d3y)
+        plt.plot(d1[:,0], d1[:,1])
+        plt.plot(d2[:,0], d2[:,1])
+        plt.plot(d3[:,0], d3[:,1])
+        
+#        def ControlPoints(self):
+#        """
+#        Here we create a matrix with all the control points, d_i
+#        """
+#        N_matrix = zeros((self.L+1, self.L+1))
+#        print(shape(N_matrix))
+#        for i in range(self.L+1):
+#            for j in range(self.L+1):
+#                N_matrix[i,j] = self.Basis(self.xi[i],3,j)
+#                
+#        " TODO: Implement scipy.linalg.solve_banded() correctly in order to solve Nd = sx and Nd = sy "
+#        " Requires creation of ab-matrix: ab[1 + i - j, j] == a[i,j]"
+#                
+#                
+#        return
+#            
+#    
+#    def basis(self,k=3,j):
+#        """
+#        Runs the recursive algorithm for the basis functions
+#        """
+#        u_grid = self.u_grid
+#        
+#        def N(self, u):        
+#            j = self.findhot(u)
+#            
+#            if k == 0:
+#                if u[j-1] == u[j]:
+#                    return 0
+#                elif u >= u[j-1] and u > u[j]:
+#                    return 1
+#                else:
+#                    return 0
+#                
+#            if u_grid[j+k-1] == u_grid[j-1]:
+#                a = 0
+#            else:
+#                a = (u-u_grid[j-1])/(u_grid[j+k-1]-u_grid[j-1])
+#            if u_grid[j+k] == u_grid[j]:
+#                b = 0
+#            else:
+#                b = (u_grid[j+k]-u)/(u_grid[j+k]-u_grid[j])  
+#  
+#                return a*basis(k-1,j) + b*basis(k-1,j+1)
+#  
+#        return N
+#    
+#    
+#class  TestBasis(unittest.TestCase):
+#    def setUp(self):
+#        self.spline = bspline(d, u_grid)
+#    def TestPositive(self):
+#        for j in range(len(self.spline.d)):
+#            N = self.spline.basis(j)
+#            for i in self.spline.u_grid:
+#                self.assertFalse( N(i) < 0 )
+#    
 
 d = [(-12.73564, 9.03455),
 (-26.77725, 15.89208),
@@ -103,5 +182,7 @@ u_grid[ 1] = u_grid[ 2] = u_grid[ 0]
 u_grid[-3] = u_grid[-2] = u_grid[-1]
 
 b = bspline(d, u_grid)
-s = b(0.2, d)
-print('s(0.2) =', s)
+s = b(0.2, d, 15)
+
+
+# Test that blossom_i < len(d)
