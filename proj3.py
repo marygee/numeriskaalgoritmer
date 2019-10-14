35d1481c105c1320ff83bda8fd02a0fa8474ffd7
marygee <38464170+marygee@users.noreply.github.com>
2019-10-14 15:04:31 +0200

    Add files via upload

---
+++ proj3.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 09:34:27 2019
+@author: selle
+"""
+from  scipy import *
+from  pylab import *
+from mpi4py import MPI
+from abc import ABC, abstractmethod
+from room import LeftSideRoom, RightSideRoom, MiddleRoom
+from scipy.linalg import toeplitz
+from numpy.linalg import solve
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nprocessors = comm.Get_size()
+#print('helloworldfromprocess' , rank)
+
+# Initialize the three rooms
+LeftSideRoom = LeftSideRoom(height = 1, width = 1, deltaX = 1/20, gammaH = 40, gammaWF = 5, gammaNormal = 15)
+RightSideRoom = RightSideRoom(height = 1, width = 1, deltaX = 1/20, gammaH = 40, gammaWF = 5, gammaNormal = 15)
+MiddleRoom = MiddleRoom(height = 2, width = 1, deltaX = 1/20, gammaH = 40, gammaWF = 5, gammaNormal = 15)
+
+class Solver:
+    
+    def __init__(self):
+        pass
+    
+    def __call__(self, room, leftBoundaryCond = None, rightBoundaryCond = None):
+        K = self.Kmatrix(room)
+        F = self.Fvector(room, leftBoundaryCond, rightBoundaryCond)
+        newroom, leftBC, rightBC = self.timeStep(room, K, F, leftBoundaryCond, rightBoundaryCond)
+        
+    def Fvector(self, room, leftBoundaryCond, rightBoundaryCond):
+        roomType = type(room).__name__
+        
+        if roomType == 'LeftSideRoom': #Normal uppe och nere, H till vänster, leftBC till höger
+            F = zeros((room.meshW-1)*(room.meshH-2)) 
+            for i in range(room.meshW-1):
+                F[i] = room.gammaNormal
+                F[-i-1] = room.gammaNormal
+            for i in range(room.meshH-2):
+                F[i*(room.meshW-1)] += room.gammaH #Vänster sida
+                F[i*(room.meshW-1) + room.meshW-2] += leftBoundaryCond[i] #Höger sida
+        
+        elif roomType == 'RightSideRoom': #Normal uppe och nere, H till höger, rightBC till vänster
+            F = zeros((room.meshW-1)*(room.meshH-2))            
+            for i in range(room.meshW-1):
+                F[i] = room.gammaNormal
+                F[-i-1] = room.gammaNormal
+            for i in range(room.meshH-2):
+                F[i*(room.meshW-1)] += rightBoundaryCond[i]  #Vänster sida
+                F[i*(room.meshW-1) + room.meshW-2] += room.gammaH #Höger sida
+
+        elif roomType == 'MiddleRoom':
+            F = zeros((room.meshW-2)*(room.meshH-2))
+            gammaNormal = room.gammaNormal*ones(int((room.meshH-1)/2))
+            leftSide = append(gammaNormal, leftBoundaryCond)
+            rightSide = append(rightBoundaryCond, gammaNormal)
+            for i in range(room.meshW-2):
+                F[i] = room.gammaH
+                F[-i-1] = room.gammaWF
+            for i in range(room.meshH-2):
+                F[i*(room.meshW-2)] += leftSide[i]
+                F[i*(room.meshW-2) + room.meshW-3] += rightSide[i]  
+            F = -F
+        else:
+            print('Room type unknown.')
+        return F
+    
+    def Kmatrix(self, room):
+        roomType = type(room).__name__
+        if roomType == 'LeftSideRoom':
+            a = zeros((room.meshW-1)*(room.meshH-2))
+            a[0] = -4
+            a[1] = 1
+            a[room.meshW-1] = 1
+            K = toeplitz(a)
+            for i in range(room.meshH-2): 
+                K[i*(room.meshW-1)+room.meshW-2, i*(room.meshW-1)+room.meshW-3] = 1 # Redandant for Maria method but crucial in case of Wiki method (if so, change to 0)
+                K[i*(room.meshW-1)+room.meshW-2, i*(room.meshW-1)+room.meshW-2] = -3 # In case of Wiki method, replace -3 with -2
+            for i in range(room.meshH-3): 
+                K[i*(room.meshW-1)+room.meshW-2, i*(room.meshW-1)+room.meshW-1] = 0
+                K[i*(room.meshW-1)+room.meshW-1, i*(room.meshW-1)+room.meshW-2] = 0
+        elif roomType == 'RightSideRoom':
+            a = zeros((room.meshW-1)*(room.meshH-2))
+            a[0] = -4
+            a[1] = 1
+            a[room.meshW-1] = 1
+            K = toeplitz(a)
+            for i in range(room.meshH-2):
+                K[i*(room.meshW-1), i*(room.meshW-1)+1] = 1 # Redandant for Maria method but crucial in case of Wiki method (if so, change to 0)
+                K[i*(room.meshW-1), i*(room.meshW-1)] = -3 # In case of Wiki method, replace -3 with -2
+            for i in range(room.meshH-3):
+                K[i*(room.meshW-1)+room.meshW-2, i*(room.meshW-1)+room.meshW-1] = 0
+                K[i*(room.meshW-1)+room.meshW-1, i*(room.meshW-1)+room.meshW-2] = 0
+        elif roomType == 'MiddleRoom':
+            a = zeros((room.meshW-2)*(room.meshH-2))
+            a[0] = -4
+            a[1] = 1
+            a[room.meshW-2] = 1
+            K = toeplitz(a)
+            for i in range(room.meshH-3):
+                K[i*(room.meshW-2)+room.meshW-3, i*(room.meshW-2)+room.meshW-2] = 0
+                K[i*(room.meshW-2)+room.meshW-2, i*(room.meshW-2)+room.meshW-3] = 0
+        else:
+            print('Room type unknown.')
+        return K
+
+    def timeStep(self, room, K, F, leftBoundaryCond, rightBoundaryCond, omega = 0.8):
+        roomType = type(room).__name__
+        
+        u = solve(K, F)
+        
+        if roomType == 'LeftSideRoom':
+            u = reshape(u, (room.meshH-2, room.meshW-1)) 
+            u_relax = omega*u + (1-omega)*room.mesh[1:-1,1:] #Relaxation
+            
+            room.UpdateMesh(u_relax)
+            
+            leftBoundaryCond = u_relax[:,-1]
+            rightBoundaryCond = None
+            
+        if roomType == 'RightSideRoom':
+            u = reshape(u, (room.meshH-2, room.meshW-1)) 
+            u_relax = omega*u + (1-omega)*room.mesh[1:-1,:-1] #Relaxation
+            
+            room.UpdateMesh(u_relax)
+                    
+            leftBoundaryCond = None
+            rightBoundaryCond = u_relax[:,0]
+            
+        if roomType == 'MiddleRoom':
+            u = reshape(u, (room.meshH-2, room.meshW-2))
+            u_relax = omega*u + (1-omega)*room.mesh[1:-1,1:-1] #Relaxation
+            leftBC_relax = omega*leftBoundaryCond + (1-omega)*room.mesh[-len(leftBoundaryCond):,0]
+            rightBC_relax = omega*rightBoundaryCond + (1-omega)*room.mesh[:len(rightBoundaryCond),-1]
+        
+            room.UpdateMesh(u_relax, leftBC_relax, rightBC_relax)         
+            
+            leftBoundaryCond -= u_relax[-len(leftBoundaryCond):,0] #Neumann condition
+            rightBoundaryCond -= u_relax[0:len(rightBoundaryCond),-1] #Neumann condition
+           
+        return room, leftBoundaryCond, rightBoundaryCond     
+
+testSolver = Solver()
+
+gamma1Middle = 22*ones(int((MiddleRoom.meshH-3)/2))
+gamma2Middle = 23*ones(int((MiddleRoom.meshH-3)/2))
+newMiddleRoom = testSolver(MiddleRoom, leftBoundaryCond = gamma1Middle, rightBoundaryCond = gamma2Middle)
+
+gammaLeft = -1*ones(int(LeftSideRoom.meshH-2))
+newLeftSideRoom = testSolver(LeftSideRoom, leftBoundaryCond = gammaLeft)
+
+gammaRight = -1*ones(int(RightSideRoom.meshH-2))
+newRightSideRoom = testSolver(RightSideRoom, rightBoundaryCond = gammaRight)
---
+++ room.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 09:34:27 2019
+@author: selle
+"""
+from  scipy import *
+from  pylab import *
+from mpi4py import MPI
+from abc import ABC, abstractmethod
+
+class Room(ABC):
+    
+    def __init__(self, height, width, deltaX, gammaH, gammaWF, gammaNormal):
+        self.height = height
+        self.width = width
+        self.deltaX = deltaX
+        self.gammaH = gammaH  # Condition for walls with heater
+        self.gammaWF = gammaWF # Condition for wals with big window
+        self.gammaNormal = gammaNormal # All walls except gammaH and gammaWF
+        self.initialTemp = 20
+        self.meshH = int(self.height/self.deltaX+1.) # Rows in mesh
+        self.meshW = int(self.width/self.deltaX+1.) # Colons in mesh
+        self.mesh = self.Discretize()
+    
+    def __call__(self):
+        """
+        Returns the mesh.
+        """
+        return self.mesh
+    
+    @abstractmethod
+    def Discretize(self):
+        """
+        Creates a mesh. 
+        """
+        pass
+    
+    @abstractmethod
+    def UpdateMesh(self):
+        """
+        Replaces the mesh with a new mesh.
+        """
+        #try:
+        #    newMesh.shape == self.mesh.shape # Checks that meshes are of same size
+        #    self.mesh = newMesh
+        #except:
+        #    print('Dimensions of new and old mesh do not match')
+        pass
+    
+class LeftSideRoom(Room):
+    
+    def Discretize(self):
+        if ((self.height/self.deltaX)%1 == 0) and ((self.width/self.deltaX)%1 == 0): # Checks that hight and width are divisible by deltaX
+            mesh = self.initialTemp*ones((self.meshH, self.meshW)) # Creates a room with correct dimensions and constant temperature
+            mesh[0,:] = self.gammaNormal # Sets boundary condition
+            mesh[-1,:] = self.gammaNormal # Sets boundary condition
+            mesh[:,0] = self.gammaH # Sets boundary condition
+            return mesh
+        else:
+            print('Height or width is not evenly divisible by deltaX')
+            
+    def UpdateMesh(self, newMesh):
+        self.mesh[1:-1,1:] = newMesh
+
+class RightSideRoom(Room):
+        
+
+    def Discretize(self):
+        if ((self.height/self.deltaX)%1 == 0) and ((self.width/self.deltaX)%1 == 0): # Checks that hight and width are divisible by deltaX
+            mesh = self.initialTemp*ones((self.meshH, self.meshW)) # Creates a room with correct dimensions and constant temperature
+            mesh[0,:] = self.gammaNormal # Sets boundary condition
+            mesh[-1,:] = self.gammaNormal # Sets boundary condition
+            mesh[:,-1] = self.gammaH # Sets boundary condition
+            return mesh
+        else:
+            print('Height or width is not evenly divisible by deltaX')
+            
+    def UpdateMesh(self, newMesh):
+        self.mesh[1:-1,:-1] = newMesh
+   
+class MiddleRoom(Room):
+
+    def Discretize(self):
+        if ((self.height/self.deltaX)%1 == 0) and ((self.width/self.deltaX)%1 == 0): # Checks that hight and width are divisible by deltaX
+            mesh = self.initialTemp*ones((self.meshH, self.meshW)) # Creates a room with correct dimensions and constant temperature
+            mesh[:,0] = self.gammaNormal # Sets boundary condition
+            mesh[:,-1] = self.gammaNormal # Sets boundary condition
+            mesh[-1,:] = self.gammaWF # Sets boundary condition
+            mesh[0,:] = self.gammaH # Sets boundary condition
+            return mesh
+        else:
+            print('Height or width is not evenly divisible by deltaX')
+            
+    def UpdateMesh(self, newMesh, leftBoundaryCondition, rightBoundaryCondition):
+        self.mesh[1:-1,1:-1] = newMesh
+        self.mesh[-len(leftBoundaryCondition):,0] = leftBoundaryCondition
+        self.mesh[:len(rightBoundaryCondition),-1] = rightBoundaryCondition 
+
+        
